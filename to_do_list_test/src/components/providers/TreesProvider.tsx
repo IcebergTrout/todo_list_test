3b67ea1123c4ea244bd45f1628d61fdc028a3663
HiveRaven3 <77645377+HiveRaven3@users.noreply.github.com>
2024-08-03 02:32:42 -0700

    hopes and dreams

--- to_do_list_test/src/components/providers/TreesProvider.tsx
+++ to_do_list_test/src/components/providers/TreesProvider.tsx
@@ -24,7 +24,7 @@ export const TreesProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   useEffect(() => {
     // Store trees data in local storage whenever it changes
 		console.log(JSON.stringify(trees));
-    localStorage.setItem('trees', JSON.stringify(trees.map(tree => tree.toJSON())));
+    if (trees.length > 0) {localStorage.setItem('trees', JSON.stringify(trees.map(tree => tree.toJSON())))};
   }, [trees]);
 
 	
